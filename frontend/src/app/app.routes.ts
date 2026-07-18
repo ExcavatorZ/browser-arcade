77faa898641a20e37d5eff644de7232234730e4f
ExcavatorZ <excavatorz@hotmail.fi>
2026-07-19 00:42:23 +0300

    Create initial game list component and configure routing.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { Homepage } from "./homepage/homepage";
+import { Games } from "./games/games";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: Homepage,
+    title: "Home | Browser Arcade",
+  },
+  {
+    path: "games",
+    component: Games,
+    title: "Game List | Browser Arcade",
+  },
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { Homepage } from "./homepage/homepage";
 import { Header } from "./header/header";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, Homepage, Header],
+  imports: [RouterOutlet, Header],
   templateUrl: "./app.html",
   styleUrl: "./app.css",
 })
