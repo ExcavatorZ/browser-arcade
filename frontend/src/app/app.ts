d1ccb1ed5f5e65b466657a5dbac868966888d579
ExcavatorZ <excavatorz@hotmail.fi>
2026-07-18 23:03:38 +0300

    Create initial header and homepage components.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { Homepage } from "./homepage/homepage";
+import { Header } from "./header/header";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+  selector: "app-root",
+  imports: [RouterOutlet, Homepage, Header],
+  templateUrl: "./app.html",
+  styleUrl: "./app.css",
 })
 export class App {
-  protected readonly title = signal('browser-arcade');
+  protected readonly title = signal("browser-arcade");
 }
