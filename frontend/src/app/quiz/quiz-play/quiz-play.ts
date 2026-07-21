5058698229aad9b040db07bb22683b5aa5c044a9
ExcavatorZ <excavatorz@hotmail.fi>
2026-07-21 23:52:41 +0300

    Condense template and apply styles according to chosen answer.

--- frontend/src/app/quiz/quiz-play/quiz-play.ts
+++ frontend/src/app/quiz/quiz-play/quiz-play.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit, signal } from "@angular/core";
 import { QuizItem, QuizService } from "../quiz.service";
 import { CommonModule } from "@angular/common";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-quiz-play",
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: "./quiz-play.html",
 })
 export class QuizPlay implements OnInit {
@@ -22,42 +23,59 @@ export class QuizPlay implements OnInit {
     this.service.getQuestions(amount, difficulty).subscribe((response) => {
       this.questions = response;
       this.currentQuestion.set(this.questions[0]);
+      this.choices.set([
+        this.questions[0].answer0,
+        this.questions[0].answer1,
+        this.questions[0].answer2,
+        this.questions[0].answer3,
+      ]);
     });
   }
 
   // Gameplay logic
 
   currentQuestion = signal<QuizItem | null>(null);
+  selectedAnswer = signal<string | null>(null);
   answered = signal(false);
   correct = signal(false);
+  choices = signal<string[]>([]);
 
   selectAnswer = (answer: string) => {
     if (this.answered()) {
       return;
     }
 
+    this.selectedAnswer.set(answer);
     this.answered.set(true);
 
-    const choices = [
-      this.currentQuestion()!.answer0,
-      this.currentQuestion()!.answer1,
-      this.currentQuestion()!.answer2,
-      this.currentQuestion()!.answer3,
-    ];
     const correct = this.currentQuestion()!.correctIndex;
 
-    for (let i = 0; i < choices.length; i++) {
-      if (answer == choices[correct]) {
-        this.score.set(this.score() + 1);
-        this.correct.set(true);
-        break;
-      }
+    if (answer == this.choices()[correct]) {
+      this.score.set(this.score() + 1);
+      this.correct.set(true);
     }
+
     this.progress.set(this.progress() + 1);
     if (this.progress() == this.questions.length) {
       this.endGame();
     }
   };
+
+  getAnswerClass(index: number, answer: string) {
+    if (!this.answered()) {
+      return "bg-indigo-200 hover:bg-indigo-300";
+    }
+
+    if (index === this.currentQuestion()!.correctIndex) {
+      return "bg-green-500 text-white";
+    }
+
+    if (answer === this.selectedAnswer()) {
+      return "bg-red-500 text-white";
+    }
+
+    return "bg-indigo-200";
+  }
 
   endGame = () => {
     this.completed.set(true);
@@ -66,6 +84,12 @@ export class QuizPlay implements OnInit {
 
   renderQuestion = () => {
     this.currentQuestion.set(this.questions[this.progress()]);
+    this.choices.set([
+      this.currentQuestion()!.answer0,
+      this.currentQuestion()!.answer1,
+      this.currentQuestion()!.answer2,
+      this.currentQuestion()!.answer3,
+    ]);
     this.answered.set(false);
     this.correct.set(false);
   };
