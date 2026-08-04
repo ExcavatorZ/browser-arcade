2d96f9559b9efcaf8a12bf53c4ef4a1cdfb6a98d
ExcavatorZ <excavatorz@hotmail.fi>
2026-08-04 20:01:16 +0300

    Dockerize application.

--- backend/Program.cs
+++ backend/Program.cs
@@ -21,9 +21,11 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+string connectionString = Environment.GetEnvironmentVariable("DefaultConnection") ?? builder.Configuration.GetConnectionString("DefaultConnection")!;
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection")
+        connectionString
         ?? throw new InvalidOperationException(
             "Missing DB connection string")));
 
@@ -68,6 +70,13 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+using (IServiceScope scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
