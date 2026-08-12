5059f6fdecc24d51aebc2ef533cafe780539313a
ExcavatorZ <excavatorz@hotmail.fi>
2026-08-12 23:52:32 +0300

    Create few tests for profile and auth services.

--- frontend/src/app/profile/profile.service.spec.ts
+++ frontend/src/app/profile/profile.service.spec.ts
@@ -1,16 +1,63 @@
 import { TestBed } from "@angular/core/testing";
 
-import { ProfileService } from "./profile.service";
+import { ProfileDetailInfo, ProfileInfo, ProfileService } from "./profile.service";
+import { provideHttpClient } from "@angular/common/http";
+import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
 
 describe("ProfileService", () => {
   let service: ProfileService;
+  let httpTesting: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(ProfileService);
+    httpTesting = TestBed.inject(HttpTestingController);
   });
 
-  it("should be created", () => {
-    expect(service).toBeTruthy();
+  it("should get profile overview", () => {
+    const profileInfo: ProfileInfo = {
+      memoryGames: 1,
+      quizGames: 2,
+      snakeGames: 3,
+      invaderGames: 4,
+    };
+
+    service.getProfileOverview("mushroom").subscribe((response) => {
+      expect(response).toEqual(profileInfo);
+    });
+
+    const req = httpTesting.expectOne(`${service.url}/overview/mushroom`);
+
+    expect(req.request.method).toBe("GET");
+
+    req.flush(profileInfo);
+  });
+
+  it("should get profile details", () => {
+    const profileDetailInfo: ProfileDetailInfo = {
+      quizGames: 1,
+      memoryGames: 2,
+      snakeGames: 3,
+      invaderGames: 4,
+      commonMemoSize: "4x4",
+      commonQuizLength: 5,
+      commonQuizDifficulty: 1,
+      commonSnakeScore: 40,
+      snakeHighScore: 56,
+      commonInvaderScore: 150,
+      invaderHighScore: 200,
+    };
+
+    service.getProfileDetails("mushroom").subscribe((response) => {
+      expect(response).toEqual(profileDetailInfo);
+    });
+
+    const req = httpTesting.expectOne(`${service.url}/details/mushroom`);
+
+    expect(req.request.method).toBe("GET");
+
+    req.flush(profileDetailInfo);
   });
 });
--- frontend/src/app/shared/auth-service.spec.ts
+++ frontend/src/app/shared/auth-service.spec.ts
@@ -1,16 +1,58 @@
 import { TestBed } from "@angular/core/testing";
 
 import { AuthService } from "./auth-service";
+import { provideHttpClient } from "@angular/common/http";
+import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
 
 describe("AuthService", () => {
   let service: AuthService;
+  let httpTesting: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(AuthService);
+    httpTesting = TestBed.inject(HttpTestingController);
   });
 
-  it("should be created", () => {
-    expect(service).toBeTruthy();
+  it("should retrieve saved token", () => {
+    localStorage.setItem("token", "test-token");
+    expect(service.getToken()).toBe("test-token");
+  });
+
+  it("should delete saved token", () => {
+    localStorage.setItem("token", "test-token");
+    service.deleteToken();
+    expect(service.getToken()).toBeNull();
+    expect(service.loggedIn()).toBeFalsy();
+  });
+
+  it("should create user", () => {
+    const formData = {
+      userName: "mushroom",
+    };
+
+    service.createUser(formData).subscribe();
+
+    const req = httpTesting.expectOne(`${service.url}/User/signup`);
+
+    expect(req.request.method).toBe("POST");
+
+    req.flush({});
+  });
+
+  it("should log in user", () => {
+    const formData = {
+      userName: "mushroom",
+    };
+
+    service.loginUser(formData).subscribe();
+
+    const req = httpTesting.expectOne(`${service.url}/User/login`);
+
+    expect(req.request.method).toBe("POST");
+
+    req.flush({});
   });
 });
