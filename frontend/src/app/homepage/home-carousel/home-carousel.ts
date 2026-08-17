77d3734016f68adeb6b1f0a112518a2f6864ab8c
ExcavatorZ <excavatorz@hotmail.fi>
2026-08-17 22:36:47 +0300

    Add images and create working carousel.

--- frontend/src/app/homepage/home-carousel/home-carousel.ts
+++ frontend/src/app/homepage/home-carousel/home-carousel.ts
@@ -7,13 +7,30 @@ import { Component, computed, OnDestroy, OnInit, signal } from "@angular/core";
 })
 export class HomeCarousel implements OnInit, OnDestroy {
   imageIndex = signal(0);
+  filepath = "assets/generic/";
   private intervalId!: number;
 
   currentImage = computed(() => this.images[this.imageIndex()]);
 
-  images = [{ title: "Learn!" }, { title: "Have fun!" }, { title: "Compete!" }];
+  images = [
+    { title: "Learn!", path: `${this.filepath}carousel_0.png`, id: 0 },
+    { title: "Have fun!", path: `${this.filepath}carousel_1.png`, id: 1 },
+    { title: "Compete!", path: `${this.filepath}carousel_2.png`, id: 2 },
+  ];
 
-  ngOnInit(): void {
+  prevImage = () => {
+    this.imageIndex.update((index) => (index - 1 + this.images.length) % this.images.length);
+    clearInterval(this.intervalId);
+    this.startCarousel();
+  };
+
+  nextImage = () => {
+    this.imageIndex.update((index) => (index + 1) % this.images.length);
+    clearInterval(this.intervalId);
+    this.startCarousel();
+  };
+
+  startCarousel = () => {
     this.intervalId = setInterval(() => {
       if (this.imageIndex() == this.images.length - 1) {
         this.imageIndex.set(0);
@@ -21,6 +38,10 @@ export class HomeCarousel implements OnInit, OnDestroy {
         this.imageIndex.set(this.imageIndex() + 1);
       }
     }, 3000);
+  };
+
+  ngOnInit(): void {
+    this.startCarousel();
   }
 
   ngOnDestroy(): void {
