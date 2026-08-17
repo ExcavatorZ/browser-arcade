debf7e574c4f10e35493df97ce8131c9a14cd2c7
ExcavatorZ <excavatorz@hotmail.fi>
2026-08-17 03:13:23 +0300

    Create tests for auth guard and interceptor.

--- frontend/src/app/shared/auth-guard.spec.ts
+++ frontend/src/app/shared/auth-guard.spec.ts
@@ -1,17 +1,41 @@
 import { TestBed } from "@angular/core/testing";
-import { CanActivateFn } from "@angular/router";
 
 import { authGuard } from "./auth-guard";
+import { provideRouter, Router } from "@angular/router";
+import { AuthService } from "./auth-service";
 
 describe("authGuard", () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-    TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+  const authService = {
+    loggedIn: vi.fn(),
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideRouter([]),
+        {
+          provide: AuthService,
+          useValue: authService,
+        },
+      ],
+    });
   });
 
-  it("should be created", () => {
-    expect(executeGuard).toBeTruthy();
+  it("should allow logged in users", () => {
+    authService.loggedIn.mockReturnValue(true);
+
+    const result = TestBed.runInInjectionContext(() => authGuard({} as any, {} as any));
+
+    expect(result).toBe(true);
+  });
+
+  it("should redirect logged out users", () => {
+    authService.loggedIn.mockReturnValue(false);
+
+    const result = TestBed.runInInjectionContext(() => authGuard({} as any, {} as any));
+
+    const router = TestBed.inject(Router);
+
+    expect(result).toEqual(router.createUrlTree(["/unauthorized"]));
   });
 });
--- frontend/src/app/shared/auth-interceptor.spec.ts
+++ frontend/src/app/shared/auth-interceptor.spec.ts
@@ -1,17 +1,60 @@
 import { TestBed } from "@angular/core/testing";
-import { HttpInterceptorFn } from "@angular/common/http";
+import {
+  HttpClient,
+  HttpInterceptorFn,
+  provideHttpClient,
+  withInterceptors,
+} from "@angular/common/http";
 
 import { authInterceptor } from "./auth-interceptor";
+import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
+import { AuthService } from "./auth-service";
 
 describe("authInterceptor", () => {
-  const interceptor: HttpInterceptorFn = (req, next) =>
-    TestBed.runInInjectionContext(() => authInterceptor(req, next));
+  let httpTesting: HttpTestingController;
+  let http: HttpClient;
+
+  const authService = {
+    getToken: vi.fn(),
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(withInterceptors([authInterceptor])),
+        provideHttpClientTesting(),
+        {
+          provide: AuthService,
+          useValue: authService,
+        },
+      ],
+    });
+
+    http = TestBed.inject(HttpClient);
+    httpTesting = TestBed.inject(HttpTestingController);
   });
 
-  it("should be created", () => {
-    expect(interceptor).toBeTruthy();
+  it("should add authorization when token exists", () => {
+    authService.getToken.mockReturnValue("test-token");
+
+    http.get("/test").subscribe();
+
+    const req = httpTesting.expectOne("/test");
+
+    expect(req.request.headers.get("Authorization")).toBe("Bearer test-token");
+
+    req.flush({});
+  });
+
+  it("should not add authorization when token doesn't exist", () => {
+    authService.getToken.mockReturnValue(null);
+
+    http.get("/test").subscribe();
+
+    const req = httpTesting.expectOne("/test");
+
+    expect(req.request.headers.has("Authorizarion")).toBe(false);
+
+    req.flush({});
   });
 });
