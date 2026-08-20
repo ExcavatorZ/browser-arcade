5774fbb8c0c9057ea90f1bb83b6ea80764717ebb
ExcavatorZ <excavatorz@hotmail.fi>
2026-08-20 10:34:17 +0300

    Implement slide animation for carousel.

--- frontend/src/app/homepage/home-carousel/home-carousel.ts
+++ frontend/src/app/homepage/home-carousel/home-carousel.ts
@@ -4,9 +4,11 @@ import { Component, computed, OnDestroy, OnInit, signal } from "@angular/core";
   selector: "app-home-carousel",
   imports: [],
   templateUrl: "./home-carousel.html",
+  styleUrl: "./home-carousel.css",
 })
 export class HomeCarousel implements OnInit, OnDestroy {
   imageIndex = signal(0);
+  direction = signal<"left" | "right">("right");
   filepath = "assets/generic/";
   private intervalId!: number;
 
@@ -19,12 +21,14 @@ export class HomeCarousel implements OnInit, OnDestroy {
   ];
 
   prevImage = () => {
+    this.direction.set("left");
     this.imageIndex.update((index) => (index - 1 + this.images.length) % this.images.length);
     clearInterval(this.intervalId);
     this.startCarousel();
   };
 
   nextImage = () => {
+    this.direction.set("right");
     this.imageIndex.update((index) => (index + 1) % this.images.length);
     clearInterval(this.intervalId);
     this.startCarousel();
@@ -32,6 +36,7 @@ export class HomeCarousel implements OnInit, OnDestroy {
 
   startCarousel = () => {
     this.intervalId = setInterval(() => {
+      this.direction.set("right");
       if (this.imageIndex() == this.images.length - 1) {
         this.imageIndex.set(0);
       } else {
